fa74a54fcc8696aacca407160808c31d88e090ad
Ekansh Bansal <ekanshban483@gmail.com>
2023-04-12 07:46:31 +0530

    day12

---
+++ Day012- Carry Look Ahead Generator/carry_look_ahead_gen.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+module carry_look_ahead_gen(
+    input [3:0] a, b,
+    input cin,
+    output [3:0] sum,
+    output carry
+    );
+wire p0, g0, p1, g1, p2, g2, p3, g3;
+wire [3:0]c;
+
+and (g0, a[0], b[0]),
+    (g1, a[1], b[1]),
+    (g2, a[2], b[2]),
+    (g3, a[3], b[3]);
+
+xor (p0, a[0], b[0]),
+    (p1, a[1], b[1]),
+    (p2, a[2], b[2]),
+    (p3, a[3], b[3]);
+    
+xor (sum[0], p0, cin),
+    (sum[1], p1, c0),
+    (sum[2], p2, c1),
+    (sum[3], p3, c2);
+
+assign  c[0] = g0 | (p0 & cin),
+        c[1] = g1 | (p1 & g0) | (p1 & p0 & cin),
+        c[2] = g2 | (p2 & g1) | (p2 & p1 & g0) | (p2 & p1 & p0 & cin),
+        c[3] = g3 | (p3 & g2) | (p3 & p2 & g1) | (p3 & p2 & p1 & g0) |(p3 & p2 & p1 & p0 & cin);
+        
+assign carry = c3;
+endmodule
---
+++ Day012- Carry Look Ahead Generator/test_bench.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+module test_bench;
+    reg [3:0] a, b;
+    reg cin;
+    wire [3:0] sum;
+    wire carry;
+    carry_look_ahead_gen dut(a, b, cin, sum, carry);
+    
+    initial
+        begin
+            a = 4'b1000; b = 4'b0011; cin = 1'b0;
+        #10 a = 4'b0001; b = 4'b1010; cin = 1'b1;
+        #10 a = 4'b0110; b = 4'b0110; cin = 1'b0;
+        #10 a = 4'b0111; b = 4'b1110; cin = 1'b0;
+        #10 a = 4'b1001; b = 4'b0110; cin = 1'b1;
+        #10 a = 4'b1001; b = 4'b0100; cin = 1'b0;
+        #10 a = 4'b1111; b = 4'b1110; cin = 1'b1;
+    end
+    initial
+    begin $monitor("a=%b b=%b cin=%b Sum=%b Carry=%b",a,b,cin,sum,carry);
+    #70 $finish;
+    end
+endmodule
